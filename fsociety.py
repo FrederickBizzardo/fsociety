0f4c81d2e19739a13c7a0ec369663e4272e7ff46
Frederick <nyembofred@gmail.com>
2022-02-05 13:56:30 +0200

    IGNORE PARENTHESES
    Sorry..please ignore my previous changes. I just found out…I needed to install python2 and not 3. Thank you…I appreciate your work!

--- fsociety.py
+++ fsociety.py
@@ -1191,9 +1191,9 @@ def grabuploadedlink(url):
         for dir in directories:
             currentcode = urllib.urlopen(url + dir).getcode()
             if currentcode == 200 or currentcode == 403:
-                print "-------------------------"
+                print "-------------------------" #please ignore the parenthesis I recently applied
                 print "  [ + ] Found Directory:  " + str(url + dir) + " [ + ]"
-                print "-------------------------"
+                print "-------------------------" #please ignore the parenthesis I recently applied
                 upload.append(url + dir)
     except:
         pass
